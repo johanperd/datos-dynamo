3d3c3905cd19d1ecd2220364e031ac81581386de
johan perdomo <165205481+JohanPerdomoSerrano@users.noreply.github.com>
2024-08-30 22:40:59 -0500

    datos

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-require('./tracing'); 
+require('./tracing');
 const express = require('express');
 const serverless = require('serverless-http');
-const logger = require('./logger'); 
+const logger = require('./logger');
+const AWS = require('aws-sdk');
 const app = express();
 
-const carros = [
-    { id: 1, nombre: 'cerato' },
-    { id: 2, nombre: 'corolla' },
-    { id: 3, nombre: 'rio' },
-    { id: 4, nombre: 'captiva' },
-    { id: 5, nombre: 'civic' },
-];
+// Configura DynamoDB
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = 'datos';
 
+app.get('/datos-dynamo', async (req, res) => {
+    try {
+        const params = {
+            TableName: TABLE_NAME
+        };
 
-app.get('/datos-dynamo', (req, res) => {
-    logger.info('Ruta /datos-dynamo accedida, retornado datos-dynamo {'+JSON.stringify(carros)+'}'); // Log de información
-    res.send(JSON.stringify(carros));
+        const data = await dynamoDb.scan(params).promise();
+        logger.info('Ruta /datos-dynamo accedida, retornando datos de DynamoDB', data.Items); // Log de información
+        res.json(data.Items);
+    } catch (error) {
+        logger.error('Error al acceder a DynamoDB', error); // Log de error
+        res.status(500).json({ error: 'No se pudieron obtener los datos de DynamoDB' });
+    }
 });
 
 // Exporta el manejador para AWS Lambda
