b163a6fe0ae0daa132fd02923791cdacd16e532b
johan perdomo <165205481+JohanPerdomoSerrano@users.noreply.github.com>
2024-09-02 17:34:15 -0500

    data

--- logger.js
+++ logger.js
@@ -1,3 +1,5 @@
+const { LogLevel, LoggerProvider, SimpleLogProcessor } = require('@opentelemetry/sdk-logs');
+const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, printf } = format;
 
@@ -5,6 +7,29 @@ const { combine, timestamp, printf } = format;
 const logFormat = printf(({ level, message, timestamp }) => {
     return `${timestamp} [${level}]: ${message}`;
 });
+
+const headers = {
+    'Authorization': 'Basic OTg2MTA2OmdsY19leUp2SWpvaU1URTNOVE0yTmlJc0ltNGlPaUpoZDNNdFlYZHpMV3hoYldKa1lTSXNJbXNpT2lJMVJFUktORFk1VDA4eGVXaHdjVTh6VlRJM1NEWlBSRElpTENKdElqcDdJbklpT2lKMWN5SjlmUT09'
+  };
+  
+  const logExporter = new OTLPLogExporter({
+    url: 'https://otlp-gateway-prod-us-east-0.grafana.net/otlp/v1/logs',
+    headers: headers
+  });
+
+
+
+  const loggerProvider = new LoggerProvider({
+    level: LogLevel.INFO,
+    resource: {
+      serviceName: 'datos-dynamo'
+    }
+  });
+  
+  loggerProvider.addLogProcessor(new SimpleLogProcessor(logExporter));
+  loggerProvider.register();
+  
+  module.exports = loggerProvider.getLogger('default');
 
 const logger = createLogger({
     level: 'info',
