b89dee3340f33fa2b9dfa698db526e88fea01beb
johan perdomo <165205481+JohanPerdomoSerrano@users.noreply.github.com>
2024-09-02 23:42:43 -0500

    wrwerwe

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const serverless = require('serverless-http');
 const logger = require('./logger');
 const AWS = require('aws-sdk');
 const app = express();
+const axios = require('axios');
+
+
 
 // Configura DynamoDB
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
@@ -11,13 +14,16 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = 'datos';
 
 app.get('/datos-dynamo', async (req, res) => {
+    
     try {
+        const dataService = await axios.get('https://vwb3xxy643.execute-api.us-east-2.amazonaws.com/data');
         const params = {
             TableName: TABLE_NAME
         };
         const data = await dynamoDb.scan(params).promise();
         logger.info('Ruta /datos-dynamo accedida, retornando datos de DynamoDB', data.Items); // Log de información
-        res.json(data.Items);
+        res.json(
+            (data.Items), dataService);
     } catch (error) {
         logger.error('Error al acceder a DynamoDB', error); // Log de error
         res.status(500).json({ error: 'No se pudieron obtener los datos de DynamoDB' + error});
